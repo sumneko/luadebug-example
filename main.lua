d849da76e4723b9b39325e699e11060283943000
最萌小汐 <sumneko@hotmail.com>
2022-07-26 13:52:28 +0800

    可以复现了

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 require "debugger":start "127.0.0.1:12306":event "wait"
-local filePath = 'C:\\temp.lua'
+local filePath = 'D:\\Github\\test\\plugin.lua'
 local text = io.open(filePath, 'r'):read('a')
 local f = load(text, '@' .. filePath)
 assert(f)
